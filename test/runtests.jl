e34b23c952920014900aeffece3787abd9376985
victor <victor.vanthilt@gmail.com>
2025-01-27 15:17:18 +0100

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,23 @@ using Test
 using TNRKit
 using TensorKit
 
-using QuadGK
+# check if user supplied args
+pat = r"(?:--group=)(\w+)"
+arg_id = findfirst(contains(pat), ARGS)
+const GROUP = if isnothing(arg_id)
+    uppercase(get(ENV, "GROUP", "ALL"))
+else
+    uppercase(only(match(pat, ARGS[arg_id]).captures))
+end
 
-include("spaces.jl") # do they give spacemismatches?
-include("ising.jl") # do they give the correct results (with the expected accuracy)?
-include("finalize.jl") # do they give the correct results (with the expected accuracy)?
+@time begin
+    if GROUP == "ALL" || GROUP == "SPACES"
+        @time include("spaces.jl") # do they give spacemismatches?
+    end
+    if GROUP == "ALL" || GROUP == "ISING"
+        @time include("ising.jl") # do they give the correct results (with the expected accuracy)?
+    end
+    if GROUP == "ALL" || GROUP == "FINALIZE"
+        @time include("finalize.jl") # do they give the correct results (with the expected accuracy)?
+    end
+end
