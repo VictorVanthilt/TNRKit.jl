33e38469d887dcfeaf2631b73771a684d33b406a
victor <victor.vanthilt@gmail.com>
2025-02-08 16:10:53 +0100

    format and remove notebook junk

--- src/models/ising.jl
+++ src/models/ising.jl
@@ -60,13 +60,13 @@ end
 function classical_Clock(q::Int64, β::Float64)
     V = ℂ^q
     A_clock = TensorMap(zeros, V ⊗ V ← V ⊗ V)
-    clock(i,j) = -cos(2π/q*(i-j))
+    clock(i, j) = -cos(2π / q * (i - j))
 
     for i in 1:q
         for j in 1:q
             for k in 1:q
                 for l in 1:q
-                    E = clock(i,j)+clock(j,k)+clock(k,l)+clock(l,i)
+                    E = clock(i, j) + clock(j, k) + clock(k, l) + clock(l, i)
                     A_clock[i, j, k, l] = exp(-β * E)
                 end
             end
