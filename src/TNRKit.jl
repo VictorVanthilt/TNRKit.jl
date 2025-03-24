e8eee4910ee60a01f2b7aef2aa3d3114abb3a81d
victor <victor.vanthilt@gmail.com>
2025-03-24 14:25:25 +0100

    temporarily get rid of `GILT` and `SloopTNR` untill they are proven to be correct

--- src/TNRKit.jl
+++ src/TNRKit.jl
@@ -13,24 +13,19 @@ include("schemes/tnrscheme.jl")
 include("schemes/trg.jl")
 include("schemes/btrg.jl")
 include("schemes/hotrg.jl")
-include("schemes/gilt.jl")
-include("schemes/gilt-tnr.jl")
 include("schemes/atrg.jl")
-include("schemes/slooptnr.jl")
 
 export TNRScheme
 export TRG, trg_convcrit
 export BTRG, btrg_convcrit
 export HOTRG, hotrg_convcrit
 export ATRG, atrg_convcrit
-export GILTTNR, gilttnr_convcrit
-export SLoopTNR
 
 export run!
 
 # models
 include("models/ising.jl")
-export classical_ising, classical_ising_symmetric, Potts_βc, Ising_βc, f_onsager
+export classical_ising, classical_ising_symmetric, potts_βc, ising_βc, f_onsager
 
 include("models/gross-neveu.jl")
 export gross_neveu_start
