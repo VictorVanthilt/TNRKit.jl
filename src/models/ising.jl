7ccb92fd53a6cb876a0e2f65709b215a174d36e6
Adwait-Naravane <adwaitnaravane@gmail.com>
2025-05-09 14:58:04 +0200

    Add Classical Ising initial tensor without Z2 symmetry

--- src/models/ising.jl
+++ src/models/ising.jl
@@ -44,9 +44,29 @@ function classical_ising_symmetric_3D(β)
     S = ℤ₂Space(0 => 1, 1 => 1)
     T = TensorMap(T_array, S ⊗ S ⊗ S ← S ⊗ S ⊗ S)
 
-    return T
+    return permute(T, ((1, 4), (5, 6, 2, 3)))
 end
 
+function classical_ising_3D(; beta, J=1.0)
+    K = beta * J
+
+    # Boltzmann weights
+    t = ComplexF64[exp(K) exp(-K); exp(-K) exp(K)]
+    r = eigen(t)
+    q = r.vectors * sqrt(LinearAlgebra.Diagonal(r.values)) * r.vectors
+
+    # local partition function tensor
+    O = zeros(2, 2, 2, 2, 2, 2)
+    O[1, 1, 1, 1, 1, 1] = 1
+    O[2, 2, 2, 2, 2, 2] = 1
+    @tensor o[-1 -2; -3 -4 -5 -6] :=
+        O[1 2; 3 4 5 6] * q[-1; 1] * q[-2; 2] * q[-3; 3] * q[-4; 4] * q[-5; 5] * q[-6; 6]
+
+    TMS = ℂ^2 ⊗ (ℂ^2)' ← ℂ^2 ⊗ ℂ^2 ⊗ (ℂ^2)' ⊗ (ℂ^2)'
+
+    return TensorMap(o, TMS)
+end
 ising_βc_3D = 1/4.51152469
 
 classical_ising_symmetric_3D() = classical_ising_symmetric_3D(ising_βc_3D)
+classical_ising_3D() = classical_ising_3D(β=ising_βc_3D)
