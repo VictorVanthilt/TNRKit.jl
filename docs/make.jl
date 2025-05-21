b140c419bce080aa2d3be687a6860ae0583f81c8
victor <victor.vanthilt@gmail.com>
2025-05-21 23:02:40 +0200

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,18 @@
+using TNRKit
 using Documenter
+using DocumenterCitations
 
-makedocs()
+bibpath = joinpath(@__DIR__, "src", "assets", "tnrkit.bib")
+bib = CitationBibliography(bibpath; style=:authoryear)
 
-deploydocs(; repo="github.com/VictorVanthilt/TNRKit.jl.git")
+makedocs(
+    sitename="Documentation",
+    pages = [
+        "Home" => "index.md"
+        "Library" => "lib/lib.md"
+    ],
+    
+    plugins = [bib]
+)
+
+# deploydocs(; repo="github.com/VictorVanthilt/TNRKit.jl.git")
