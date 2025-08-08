8391f9d88b5541b0bcea33b113ab8d2f6873ddba
victor <victor.vanthilt@gmail.com>
2025-08-08 11:33:22 +0200

    update test running

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,5 @@ using QuadGK
 
 include("spaces.jl") # do they give spacemismatches?
 include("schemes.jl") # do they give the correct results (with the expected accuracy)?
-include("finalize.jl") # do they give the correct results (with the expected accuracy)?
+include("models.jl") # do they give the correct results (with the expected accuracy)?
+include("fermions.jl") # do they give the correct results (with the expected accuracy)?
