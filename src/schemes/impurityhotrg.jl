e16c3bdc421f8633eb1a58e0b081e264edefb281
Adwait-Naravane <adwaitnaravane@gmail.com>
2025-10-04 12:32:00 +0200

    Modify Impurity HOTRG to work for the Potts model

--- src/schemes/impurityhotrg.jl
+++ src/schemes/impurityhotrg.jl
@@ -27,22 +27,24 @@ $(TYPEDFIELDS)
 
 mutable struct ImpurityHOTRG <: TNRScheme
     T::TensorMap
-    T_imp_1::TensorMap
-    T_imp_2::TensorMap
+    T_imp_order1_1::TensorMap
+    T_imp_order1_2::TensorMap
+    T_imp_order2::TensorMap
     finalize!::Function
     function ImpurityHOTRG(
         T::TensorMap{E,S,2,2},
-        T_imp_1::TensorMap{E,S,2,2},
-        T_imp_2::TensorMap{E,S,2,2},
+        T_imp_order1_1::TensorMap{E,S,2,2},
+        T_imp_order1_2::TensorMap{E,S,2,2}=T_imp_order1_1,
+        T_imp_order2::TensorMap{E,S,2,2},
         ;
-        finalize = (finalize!),
+        finalize=(finalize!),
     ) where {E,S}
 
-        @assert space(T, 1) == space(T_imp_1, 1) == space(T_imp_2, 1) "First index space of T, T_imp_1 and T_imp_2 must be the same"
-        @assert space(T, 2) == space(T_imp_1, 2) == space(T_imp_2, 2) "Second index space of T, T_imp_1 and T_imp_2 must be the same"
-        @assert space(T, 3) == space(T_imp_1, 3) == space(T_imp_2, 3) "Third index space of T, T_imp_1 and T_imp_2 must be the same"
-        @assert space(T, 4) == space(T_imp_1, 4) == space(T_imp_2, 4) "Fourth index space of T, T_imp_1 and T_imp_2 must be the same"
-        return new(T, T_imp_1, T_imp_2, finalize)
+        @assert space(T, 1) == space(T_imp_order1_1, 1) == space(T_imp_order1_2, 1) "First index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 2) == space(T_imp_order1_1, 2) == space(T_imp_order1_2, 2) "Second index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 3) == space(T_imp_order1_1, 3) == space(T_imp_order1_2, 3) "Third index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 4) == space(T_imp_order1_1, 4) == space(T_imp_order1_2, 4) "Fourth index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        return new(T, T_imp_order1_1, T_imp_order1_2, T_imp_order2, finalize)
     end
 end
 
@@ -55,8 +57,8 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
         conj(scheme.T[-4 3; 6 4])
 
     # get unitaries
-    U, _, _, εₗ = tsvd(MMdag; trunc = trunc)
-    _, _, Uᵣ, εᵣ = tsvd(adjoint(MMdag); trunc = trunc)
+    U, _, _, εₗ = tsvd(MMdag; trunc=trunc)
+    _, _, Uᵣ, εᵣ = tsvd(adjoint(MMdag); trunc=trunc)
 
     if εₗ > εᵣ
         U = adjoint(Uᵣ)
@@ -66,9 +68,9 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
     @tensor T[-1 -2; -3 -4] :=
         scheme.T[1 5; -3 3] * conj(U[1 2; -1]) * U[3 4; -4] * scheme.T[2 -2; 5 4]
 
-    @tensor T_imp1[-1 -2; -3 -4] :=
+    @tensor T_imp_order1_1[-1 -2; -3 -4] :=
         1 / 2 *
-        scheme.T_imp_1[1 5; -3 3] *
+        scheme.T_imp_order1_1[1 5; -3 3] *
         conj(U[1 2; -1]) *
         U[3 4; -4] *
         scheme.T[2 -2; 5 4] +
@@ -76,11 +78,23 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
         scheme.T[1 5; -3 3] *
         conj(U[1 2; -1]) *
         U[3 4; -4] *
-        scheme.T_imp_1[2 -2; 5 4]
+        scheme.T_imp_order1_1[2 -2; 5 4]
 
-    @tensor T_imp2[-1 -2; -3 -4] :=
+    @tensor T_imp_order1_2[-1 -2; -3 -4] :=
+        1 / 2 *
+        scheme.T_imp_order1_2[1 5; -3 3] *
+        conj(U[1 2; -1]) *
+        U[3 4; -4] *
+        scheme.T[2 -2; 5 4] +
+        1 / 2 *
+        scheme.T[1 5; -3 3] *
+        conj(U[1 2; -1]) *
+        U[3 4; -4] *
+        scheme.T_imp_order1_2[2 -2; 5 4]
+
+    @tensor T_imp_order2[-1 -2; -3 -4] :=
         1 / 4 *
-        scheme.T_imp_2[1 5; -3 3] *
+        scheme.T_imp_order2[1 5; -3 3] *
         conj(U[1 2; -1]) *
         U[3 4; -4] *
         scheme.T[2 -2; 5 4] +
@@ -88,16 +102,22 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
         scheme.T[1 5; -3 3] *
         conj(U[1 2; -1]) *
         U[3 4; -4] *
-        scheme.T_imp_2[2 -2; 5 4] +
-        1 / 2 *
-        scheme.T_imp_1[1 5; -3 3] *
+        scheme.T_imp_order2[2 -2; 5 4] +
+        1 / 4 *
+        scheme.T_imp_order1_1[1 5; -3 3] *
         conj(U[1 2; -1]) *
         U[3 4; -4] *
-        scheme.T_imp_1[2 -2; 5 4]
+        scheme.T_imp_order1_2[2 -2; 5 4]
+    +1 / 4 *
+    scheme.T_imp_order1_2[1 5; -3 3] *
+    conj(U[1 2; -1]) *
+    U[3 4; -4] *
+    scheme.T_imp_order1_1[2 -2; 5 4]
 
     scheme.T = T
-    scheme.T_imp_1 = T_imp1
-    scheme.T_imp_2 = T_imp2
+    scheme.T_imp_order1_1 = T_imp_order1_1
+    scheme.T_imp_order1_2 = T_imp_order1_2
+    scheme.T_imp_order2 = T_imp_order2
 
     # join horizontally
     @tensor MMdag[-1 -2; -3 -4] :=
@@ -107,8 +127,8 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
         conj(scheme.T[6 -4; 4 3])
 
     # get unitaries
-    U, _, _, εₗ = tsvd(MMdag; trunc = trunc)
-    _, _, Uᵣ, εᵣ = tsvd(adjoint(MMdag); trunc = trunc)
+    U, _, _, εₗ = tsvd(MMdag; trunc=trunc)
+    _, _, Uᵣ, εᵣ = tsvd(adjoint(MMdag); trunc=trunc)
 
     if εₗ > εᵣ
         U = adjoint(Uᵣ)
@@ -118,38 +138,56 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
     @tensor T[-1 -2; -3 -4] :=
         scheme.T[-1 1; 3 5] * scheme.T[5 2; 4 -4] * conj(U[1 2; -2]) * U[3 4; -3]
 
-    @tensor T_imp1[-1 -2; -3 -4] :=
+    @tensor T_imp_order1_1[-1 -2; -3 -4] :=
         1 / 2 *
-        scheme.T_imp_1[-1 1; 3 5] *
+        scheme.T_imp_order1_1[-1 1; 3 5] *
         scheme.T[5 2; 4 -4] *
         conj(U[1 2; -2]) *
         U[3 4; -3] +
         1 / 2 *
         scheme.T[-1 1; 3 5] *
-        scheme.T_imp_1[5 2; 4 -4] *
+        scheme.T_imp_order1_1[5 2; 4 -4] *
+        conj(U[1 2; -2]) *
+        U[3 4; -3]
+    
+    @tensor T_imp_order1_2[-1 -2; -3 -4] :=
+        1 / 2 *
+        scheme.T_imp_order1_2[-1 1; 3 5] *
+        scheme.T[5 2; 4 -4] *
+        conj(U[1 2; -2]) *
+        U[3 4; -3] +
+        1 / 2 *
+        scheme.T[-1 1; 3 5] *
+        scheme.T_imp_order1_2[5 2; 4 -4] *
         conj(U[1 2; -2]) *
         U[3 4; -3]
 
-    @tensor T_imp2[-1 -2; -3 -4] :=
+    @tensor T_imp_order2[-1 -2; -3 -4] :=
         1 / 4 *
-        scheme.T_imp_2[-1 1; 3 5] *
+        scheme.T_imp_order2[-1 1; 3 5] *
         scheme.T[5 2; 4 -4] *
         conj(U[1 2; -2]) *
         U[3 4; -3] +
         1 / 4 *
         scheme.T[-1 1; 3 5] *
-        scheme.T_imp_2[5 2; 4 -4] *
+        scheme.T_imp_order2[5 2; 4 -4] *
         conj(U[1 2; -2]) *
         U[3 4; -3] +
-        1 / 2 *
-        scheme.T_imp_1[-1 1; 3 5] *
-        scheme.T_imp_1[5 2; 4 -4] *
+        1 / 4 *
+        scheme.T_imp_order1_1[-1 1; 3 5] *
+        scheme.T_imp_order1_2[5 2; 4 -4] *
         conj(U[1 2; -2]) *
         U[3 4; -3]
+    +1 / 4 *
+    scheme.T_imp_order1_2[-1 1; 3 5] *
+    scheme.T_imp_order1_1[5 2; 4 -4] *
+    conj(U[1 2; -2]) *
+    U[3 4; -3]
 
     scheme.T = T
-    scheme.T_imp_1 = T_imp1
-    scheme.T_imp_2 = T_imp2
+    scheme.T_imp_order1_1 = T_imp_order1_1
+    scheme.T_imp_order1_2 = T_imp_order1_2
+    scheme.T_imp_order2 = T_imp_order2
 
     return scheme
 end
@@ -157,7 +195,8 @@ end
 function Base.show(io::IO, scheme::ImpurityHOTRG)
     println(io, "ImpurityHOTRG - Impurity Higher Order TRG")
     println(io, "  * T: $(summary(scheme.T))")
-    println(io, "  * T_imp_1: $(summary(scheme.T_imp_1))")
-    println(io, "  * T_imp_2: $(summary(scheme.T_imp_2))")
+    println(io, "  * T_imp_order1_1: $(summary(scheme.T_imp_order1_1))")
+    println(io, "  * T_imp_order1_2: $(summary(scheme.T_imp_order1_2))")
+    println(io, "  * T_imp_order2: $(summary(scheme.T_imp_order2))")
     return nothing
 end
