6f972bde5627c3a8101a6ee721ff17d1b08fe96e
Adwait-Naravane <adwaitnaravane@gmail.com>
2025-10-05 14:43:25 +0200

    Fix more

--- src/models/potts.jl
+++ src/models/potts.jl
@@ -97,8 +97,8 @@ classical_potts_symmetric(q::Int) = classical_potts_symmetric(q, potts_βc(q))
 
 function classical_potts_impurity(q::Int64, β::Float64, k1::Int64=1, k2::Int64=1)
     bond_tensor = zeros(ComplexF64, q, q)
-    for i = 1:q
-        bond_tensor[i, i] = sqrt(exp(β) + 1 + q * (i == 1))
+    for i = 0:q-1
+        bond_tensor[i+1, i+1] = sqrt(exp(β) + 1 + q * (i == 1))
     end
     Vp = ℂ^q
     bond_tensor = TensorMap(bond_tensor, Vp ← Vp)
