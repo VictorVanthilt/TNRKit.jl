90e9f32c2ad13d58ea475f4586ba69186be367cd
victor <victor.vanthilt@gmail.com>
2025-10-17 10:17:01 +0200

    remove double definition of HOTRG projectors, small formatting changes

--- src/schemes/impurityhotrg.jl
+++ src/schemes/impurityhotrg.jl
@@ -40,123 +40,17 @@ mutable struct ImpurityHOTRG <: TNRScheme
             finalize = (finalize!),
         ) where {E, S}
 
-        @assert space(T, 1) == space(T_imp_order1_1, 1) == space(T_imp_order1_2, 1) "First index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
-        @assert space(T, 2) == space(T_imp_order1_1, 2) == space(T_imp_order1_2, 2) "Second index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
-        @assert space(T, 3) == space(T_imp_order1_1, 3) == space(T_imp_order1_2, 3) "Third index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
-        @assert space(T, 4) == space(T_imp_order1_1, 4) == space(T_imp_order1_2, 4) "Fourth index space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 1) == space(T_imp_order1_1, 1) == space(T_imp_order1_2, 1) "First space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 2) == space(T_imp_order1_1, 2) == space(T_imp_order1_2, 2) "Second space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 3) == space(T_imp_order1_1, 3) == space(T_imp_order1_2, 3) "Third space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
+        @assert space(T, 4) == space(T_imp_order1_1, 4) == space(T_imp_order1_2, 4) "Fourth space of T, T_imp_order1_1 and T_imp_order1_2 must be the same"
         return new(T, T_imp_order1_1, T_imp_order1_2, T_imp_order2, finalize)
     end
 end
 
-function _step_hotrg_x(
-        A1::TensorMap{E, S, 2, 2}, A2::TensorMap{E, S, 2, 2},
-        U::TensorMap{E, S, 2, 1}
-    ) where {E, S}
-    #= compression along the x-direction
-                -3
-                |
-            ┌3--U--4┐
-            |       |
-        -1--A1--5---A2-- -4
-            |       |
-            └1--U†-2┘
-                |
-                -2
-    =#
-
-    @tensor T[-1 -2; -3 -4] :=
-        A1[-1 1; 3 5] * A2[5 2; 4 -4] * conj(U[1 2; -2]) * U[3 4; -3]
-    return T
-end
-
-function _step_hotrg_y(
-        A1::TensorMap{E, S, 2, 2}, A2::TensorMap{E, S, 2, 2},
-        U::TensorMap{E, S, 2, 1}
-    ) where {E, S}
-    #= compression along the y-direction
-                    -3
-                    |
-            ┌---1---A2---3--┐
-            |       |       |
-        -1--U†      5       U-- -4
-            |       |       |
-            └---2---A1---4--┘
-                    |
-                    -2
-    =#
-
-    @tensor T[-1 -2; -3 -4] :=
-        conj(U[1 2; -1]) * U[3 4; -4] * A2[1 5; -3 3] * A1[2 -2; 5 4]
-    return T
-end
-
-function _get_hotrg_xproj(
-        A1::TensorMap{E, S, 2, 2}, A2::TensorMap{E, S, 2, 2},
-        trunc::TensorKit.TruncationScheme
-    ) where {E, S}
-    #= join in y-direction, keep x-indices open (A1 below A2)
-    M M†                        M† M
-            ┌---1---┐                   ┌---1---┐
-            ↓       ↑                   ↑       ↓
-    -1 -←--A2-←-2--A2†-←- -3    -1 -←--A2†--2-←-A2-←- -3
-            ↓       ↑                   ↑       ↓
-            5       6                   5       6
-            ↓       ↑                   ↑       ↓
-    -2 -←--A1-←-4--A1†-←- -4    -2 -←--A1†--4-←-A1-←- -4
-            ↓       ↑                   ↑       ↓
-            └---3---┘                   └---3---┘
-    =#
-    # get left unitary
-    @plansor MM[-1 -2; -3 -4] :=
-        A2[-1 5; 1 2] * A1[-2 3; 5 4] *
-        conj(A2[-3 6; 1 2]) * conj(A1[-4 3; 6 4])
-    U, s, _, ε = tsvd!(MM; trunc)
-    # get right unitary
-    @plansor MM[-1 -2; -3 -4] :=
-        conj(A2[2 5; 1 -1]) * conj(A1[4 3; 5 -2]) *
-        A2[2 6; 1 -3] * A1[4 3; 6 -4]
-    _, s′, U′, ε′ = tsvd!(MM; trunc)
-    if ε > ε′
-        U, s, ε = adjoint(U′), s′, ε′
-    end
-    return U, s, ε
-end
-
-function _get_hotrg_yproj(
-        A1::TensorMap{E, S, 2, 2}, A2::TensorMap{E, S, 2, 2},
-        trunc::TensorKit.TruncationScheme
-    ) where {E, S}
-    #= join in x-direction, keep y-indices open (A1 on the left of A2)
-    M M†                        M† M
-            -3      -4              -3      -4
-            ↓       ↓               ↓       ↓
-        ┌-→-A1†--6-→A2†-→┐      ┌-←-A1-←-6--A2-←-┐
-        ↑   ↓       ↓    ↓      ↑   ↓       ↓    ↓
-        1   2       4    3      1   2       4    3
-        ↑   ↓       ↓    ↓      ↑   ↓       ↓    ↓
-        └-←-A1-←-5--A2-←-┘      └-→-A1†--5-→A2†-→┘
-            ↓       ↓               ↓       ↓
-            -1      -2              -1      -2
-    =#
-    # get bottom unitary
-    @plansor MM[-1 -2; -3 -4] :=
-        A1[1 -1; 2 5] * A2[5 -2; 4 3] *
-        conj(A1[1 -3; 2 6]) * conj(A2[6 -4; 4 3])
-    U, s, _, ε = tsvd!(MM; trunc)
-    # get top unitary
-    @plansor MM[-1 -2; -3 -4] :=
-        conj(A1[1 2; -1 5]) * conj(A2[5 4; -2 3]) *
-        A1[1 2; -3 6] * A2[6 4; -4 3]
-    _, s′, U′, ε′ = tsvd!(MM; trunc)
-    if ε > ε′
-        U, s, ε = adjoint(U′), s′, ε′
-    end
-    return U, s, ε
-end
-
-
 function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
     Uy, _ = _get_hotrg_yproj(scheme.T, scheme.T, trunc)
+
     T = _step_hotrg_x(scheme.T, scheme.T, Uy)
     T_imp_order1_1 = 0.5 * (_step_hotrg_x(scheme.T_imp_order1_1, scheme.T, Uy) + _step_hotrg_x(scheme.T, scheme.T_imp_order1_1, Uy))
     T_imp_order1_2 = 0.5 * (_step_hotrg_x(scheme.T_imp_order1_2, scheme.T, Uy) + _step_hotrg_x(scheme.T, scheme.T_imp_order1_2, Uy))
@@ -166,11 +60,14 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
             _step_hotrg_x(scheme.T_imp_order1_1, scheme.T_imp_order1_2, Uy) +
             _step_hotrg_x(scheme.T_imp_order1_2, scheme.T_imp_order1_1, Uy)
     )
+
     scheme.T = T
     scheme.T_imp_order1_1 = T_imp_order1_1
     scheme.T_imp_order1_2 = T_imp_order1_2
     scheme.T_imp_order2 = T_imp_order2
+
     Ux, _ = _get_hotrg_xproj(scheme.T, scheme.T, trunc)
+
     T = _step_hotrg_y(scheme.T, scheme.T, Ux)
     T_imp_order1_1 = 0.5 * (_step_hotrg_y(scheme.T_imp_order1_1, scheme.T, Ux) + _step_hotrg_y(scheme.T, scheme.T_imp_order1_1, Ux))
     T_imp_order1_2 = 0.5 * (_step_hotrg_y(scheme.T_imp_order1_2, scheme.T, Ux) + _step_hotrg_y(scheme.T, scheme.T_imp_order1_2, Ux))
@@ -180,6 +77,7 @@ function step!(scheme::ImpurityHOTRG, trunc::TensorKit.TruncationScheme)
             _step_hotrg_y(scheme.T_imp_order1_1, scheme.T_imp_order1_2, Ux) +
             _step_hotrg_y(scheme.T_imp_order1_2, scheme.T_imp_order1_1, Ux)
     )
+
     scheme.T = T
     scheme.T_imp_order1_1 = T_imp_order1_1
     scheme.T_imp_order1_2 = T_imp_order1_2
