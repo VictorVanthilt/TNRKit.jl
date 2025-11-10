2f3e5044674969c272cf4ca2ede6204693cf05fc
sanderdemeyer <sander.demeyer@ugent.be>
2025-11-10 11:08:39 +0100

    small updates to C6vCTM
    add docs
    change inverse flips to forward flips
    improve warnings
    throw warning for fermions

--- src/schemes/ctm/c6vctm_triangular.jl
+++ src/schemes/ctm/c6vctm_triangular.jl
@@ -1,3 +1,39 @@
+"""
+$(TYPEDEF)
+
+C6v symmetric Corner Transfer Matrix Renormalization Group
+
+### Constructors
+    $(FUNCTIONNAME)(T)
+    $(FUNCTIONNAME)(T, [, symmetrize=false])
+
+     (120°)     (60°)
+        ╲       ╱
+         ╲     ╱
+          ╲   ╱
+(180°)----- T -----(0°)
+           ╱ ╲
+          ╱   ╲
+         ╱     ╲
+      (240°) (300°)
+
+c6vCTM can be called with a (3, 3) tensor, where the directions are (180°, 240°, 300°, 120°, 60°, 0°) clockwise with respect to the positive x-axis.
+In the flipped arrow convention, the arrows point from (120°, 60°, 0°) to (180°, 240°, 300°).
+or with a (0,6) tensor (120°, 60°, 0°, 300°, 240°, 180°) where all arrows point inward (unflipped arrow convention).
+The keyword argument symmetrize makes the tensor C6v symmetric when set to true. If symmetrize = false, it checks the symmetry explicitly.
+
+### Running the algorithm
+    run!(::c6vCTM, trunc::TensorKit.TruncationSheme, stop::Stopcrit[, finalize_beginning=true, verbosity=1])
+
+!!! info "verbosity levels"
+    - 0: No output
+    - 1: Print information at start and end of the algorithm
+    - 2: Print information at each step
+
+### Fields
+
+$(TYPEDFIELDS)
+"""
 mutable struct c6vCTM_triangular{A, S}
     T::TensorMap{A, S, 0, 6}
     C::TensorMap{A, S, 2, 1}
@@ -7,7 +43,9 @@ mutable struct c6vCTM_triangular{A, S}
     function c6vCTM_triangular(T::TensorMap{A, S, 0, 6}) where {A, S}
         C, Ea, Eb = c6vCTM_triangular_init(T)
 
-        @assert BraidingStyle(sectortype(T)) == Bosonic() "$(summary(BraidingStyle(sectortype(T)))) braiding style is not supported for c6vCTM"
+        if BraidingStyle(sectortype(T)) != Bosonic()
+            @warn "$(summary(BraidingStyle(sectortype(T)))) braiding style is not supported for c6vCTM"
+        end
         return new{A, S}(T, C, Ea, Eb)
     end
 end
@@ -18,8 +56,8 @@ function c6vCTM_triangular(T_flipped::TensorMap{A, S, 3, 3}; symmetrize = false)
     if symmetrize
         T_unflipped = symmetrize_C6v(T_unflipped)
     else
-        @assert norm(T_flipped - T_flipped') < 1.0e-14
-        @assert norm(T_unflipped - rotl60_pf(T_unflipped)) < 1.0e-14
+        @assert norm(T_flipped - T_flipped') < 1.0e-14 "Tensor is not hermitian. Error = $(norm(T_flipped - T_flipped'))"
+        @assert norm(T_unflipped - rotl60_pf(T_unflipped)) < 1.0e-14 "Tensor is not C6 symmetric. Error = $(norm(T_unflipped - rotl60_pf(T_unflipped)))"
     end
     return c6vCTM_triangular(T_unflipped)
 end
@@ -84,7 +122,7 @@ function step!(scheme::c6vCTM_triangular, trunc)
     ρ = build_double_corner_matrix_triangular(scheme)
     # avoid symmetry breaking due to numerical accuracy
     # mat = 0.5 * (mat + adjoint(mat))
-    @tensor ρρ[-1 -2; -3 -4] := ρ[-1 -2; 1 2] * flip(ρ, 2; inv = true)[1 2; -3 -4]
+    @tensor ρρ[-1 -2; -3 -4] := ρ[-1 -2; 1 2] * flip(ρ, 2; inv = false)[1 2; -3 -4]
 
     U, S, V = tsvd(ρρ; trunc = trunc & truncbelow(1.0e-20))
 
@@ -94,7 +132,7 @@ function step!(scheme::c6vCTM_triangular, trunc)
     Ẽa, Ẽb, Ẽatr, Ẽbtr = semi_renormalize(scheme, Pa, Pb, trunc)
 
     @tensor opt = true scheme.C[-1 -2; -3] := scheme.C[1 3; 6] * scheme.Ea[4 2; 1] * scheme.Eb[6 7; 8] *
-        flip(scheme.T, (3, 4, 5); inv = true)[3 7 9 -2 5 2] * Pa[-1; 4 5] * Pb[8 9; -3]
+        flip(scheme.T, (3, 4, 5); inv = false)[3 7 9 -2 5 2] * Pa[-1; 4 5] * Pb[8 9; -3]
 
 
     Qa, Qb = build_matrix_second_projector(scheme, Ẽa, Ẽb, Ẽatr, Ẽbtr)
@@ -118,7 +156,6 @@ function network_value_triangular(scheme::c6vCTM_triangular)
 end
 
 function lnz(scheme::c6vCTM_triangular)
-    show(scheme)
     return real(log(network_value_triangular(scheme)))
 end
 
@@ -135,7 +172,7 @@ end
 
 function semi_renormalize(scheme::c6vCTM_triangular, Pa, Pb, trunc)
     @tensor opt = true mat[-1 -2; -3 -4] := Pa[-1; 1 2] * Pb[6 7; -3] *
-        scheme.Ea[1 3; 4] * scheme.Eb[4 5; 6] * flip(scheme.T, (3, 4, 5, 6); inv = true)[3 5 7 -4 -2 2]
+        scheme.Ea[1 3; 4] * scheme.Eb[4 5; 6] * flip(scheme.T, (3, 4, 5, 6); inv = false)[3 5 7 -4 -2 2]
     U, S, V = tsvd(mat)
     Ẽb = U * sqrt(S)
     Ẽa = permute(sqrt(S) * V, ((1, 3), (2,)))
@@ -180,7 +217,7 @@ end
 # Expectation_values
 
 function _contract_edges_0(scheme)
-    return @tensor opt = true flip(scheme.T, 3; inv = true)[DL120 DL60 DL0 DL300 DL240 DL180] * scheme.T[DR120 DR60 DR0 DR300 DR240 DL0] *
+    return @tensor opt = true flip(scheme.T, 3; inv = false)[DL120 DL60 DL0 DL300 DL240 DL180] * scheme.T[DR120 DR60 DR0 DR300 DR240 DL0] *
         scheme.C[χNW DL120; χNa] * scheme.C[χNb DR60; χNE] * scheme.C[χNE DR0; χSE] *
         scheme.C[χSE DR300; χSa] * scheme.C[χSb DL240; χSW] * scheme.C[χSW DL180; χNW] *
         scheme.Eb[χNa DL60; χNC] * scheme.Ea[χNC DR120; χNb] *
@@ -188,7 +225,7 @@ function _contract_edges_0(scheme)
 end
 
 function _contract_edges_60(scheme)
-    return @tensor opt = true flip(scheme.T, 5; inv = true)[DTR120 DTR60 DTR0 DTR300 DBL60 DTR180] * scheme.T[DBL120 DBL60 DBL0 DBL300 DBL240 DBL180] *
+    return @tensor opt = true flip(scheme.T, 5; inv = false)[DTR120 DTR60 DTR0 DTR300 DBL60 DTR180] * scheme.T[DBL120 DBL60 DBL0 DBL300 DBL240 DBL180] *
         scheme.C[χNWb DTR120; χN] * scheme.C[χN DTR60; χNE] * scheme.C[χNE DTR0; χSEa] *
         scheme.C[χSEb DBL300; χS] * scheme.C[χS DBL240; χSW] * scheme.C[χSW DBL180; χNWa] *
         scheme.Eb[χSEa DTR300; χSEC] * scheme.Ea[χSEC DBL0; χSEb] *
@@ -196,7 +233,7 @@ function _contract_edges_60(scheme)
 end
 
 function _contract_edges_120(scheme)
-    return @tensor opt = true flip(scheme.T, 4; inv = true)[DTL120 DTL60 DTL0 DTL300 DTL240 DTL180] * scheme.T[DTL300 DBR60 DBR0 DBR300 DBR240 DBR180] *
+    return @tensor opt = true flip(scheme.T, 4; inv = false)[DTL120 DTL60 DTL0 DTL300 DTL240 DTL180] * scheme.T[DTL300 DBR60 DBR0 DBR300 DBR240 DBR180] *
         scheme.C[χNW DTL120; χN] * scheme.C[χN DTL60; χNEa] * scheme.C[χNEb DBR0; χSE] *
         scheme.C[χSE DBR300; χS] * scheme.C[χS DBR240; χSWa] * scheme.C[χSWb DTL180; χNW] *
         scheme.Eb[χNEa DTL0; χNEC] * scheme.Ea[χNEC DBR60; χNEb] *
@@ -204,9 +241,9 @@ function _contract_edges_120(scheme)
 end
 
 function _contract_site_large(scheme)
-    return @tensor opt = true flip(scheme.T, 5; inv = true)[DNW120 DNW60 DNW0 DNW300 DW60 DNW180] * flip(scheme.T, 6; inv = true)[DNE120 DNE60 DNE0 DNE300 DNE240 DNW0] *
-        flip(scheme.T, 1; inv = true)[DNE300 DE60 DE0 DE300 DE240 DE180] * flip(scheme.T, 2; inv = true)[DSE120 DE240 DSE0 DSE300 DSE240 DSE180] * flip(scheme.T, 3; inv = true)[DSW120 DSW60 DSE180 DSW300 DSW240 DSW180] *
-        flip(scheme.T, 4; inv = true)[DW120 DW60 DW0 DSW120 DW240 DW180] * flip(scheme.T, (1, 2, 3, 4, 5, 6); inv = true)[DNW300 DNE240 DE180 DSE120 DSW60 DW0] *
+    return @tensor opt = true flip(scheme.T, 5; inv = false)[DNW120 DNW60 DNW0 DNW300 DW60 DNW180] * flip(scheme.T, 6; inv = false)[DNE120 DNE60 DNE0 DNE300 DNE240 DNW0] *
+        flip(scheme.T, 1; inv = false)[DNE300 DE60 DE0 DE300 DE240 DE180] * flip(scheme.T, 2; inv = false)[DSE120 DE240 DSE0 DSE300 DSE240 DSE180] * flip(scheme.T, 3; inv = false)[DSW120 DSW60 DSE180 DSW300 DSW240 DSW180] *
+        flip(scheme.T, 4; inv = false)[DW120 DW60 DW0 DSW120 DW240 DW180] * flip(scheme.T, (1, 2, 3, 4, 5, 6); inv = false)[DNW300 DNE240 DE180 DSE120 DSW60 DW0] *
         scheme.C[χNWa DNW120; χNb] * scheme.Eb[χNb DNW60; χNC] * scheme.Ea[χNC DNE120; χNa] *
         scheme.C[χNa DNE60; χNEb] * scheme.Eb[χNEb DNE0; χNEC] * scheme.Ea[χNEC DE60; χNEa] *
         scheme.C[χNEa DE0; χSEb] * scheme.Eb[χSEb DE300; χSEC] * scheme.Ea[χSEC DSE0; χSEa] *
