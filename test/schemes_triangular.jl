dca5c2d5a19682c46df9ace4b18b6a2447d21fb1
sanderdemeyer <sander.demeyer@ugent.be>
2025-11-12 10:55:32 +0100

    improve tests
    require higher accuracy for tests to pass
    include triangular tests in `runtests.jl`

--- test/schemes_triangular.jl
+++ test/schemes_triangular.jl
@@ -1,6 +1,3 @@
-# lz_onsager_triangular = 0.8796
-# fs_onsager_triangular = -lz_onsager_triangular / ising_βc_triangular  # Approximate value for triangular lattice
-
 # c6CTM
 @testset "c6CTM - Ising Model" begin
     for method in [classical_ising_triangular classical_ising_triangular_symmetric]
@@ -10,6 +7,6 @@
         lz = run!(scheme, truncdim(24), maxiter(50))
 
         fs = lz * -1 / ising_βc_triangular
-        @test fs ≈ f_onsager_triangular rtol = 1.0e-3
+        @test fs ≈ f_onsager_triangular rtol = 1.0e-4
     end
 end
