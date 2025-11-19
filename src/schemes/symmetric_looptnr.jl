20c9473ae693d6ce1bc7b68d3116e3172c037ed6
victor <victor.vanthilt@gmail.com>
2025-11-19 08:35:07 +0100

    fix constructor slooptnr

--- src/schemes/symmetric_looptnr.jl
+++ src/schemes/symmetric_looptnr.jl
@@ -21,13 +21,13 @@ $(TYPEDFIELDS)
 * [Yang et. al. Phys. Rev. Letters 118 (2017)](@cite yangLoopOptimizationTensor2017) (Fig. S6)
 
 """
-mutable struct SLoopTNR{E, S, TT <: AbstractTensorMap{E, S, 2, 2}} <: TNRScheme{E, S}
+mutable struct SLoopTNR{E, S, TT <: AbstractTensorMap{E, S, 4, 0}} <: TNRScheme{E, S}
     "Central tensor"
     T::TT
 
     "Gradient optimization algorithm"
     gradalg::OptimKit.LBFGS
-    function SLoopTNR(T::TT; gradalg = LBFGS(10; verbosity = 0, gradtol = 6.0e-7, maxiter = 40000)) where {E, S, TT <: AbstractTensorMap{E, S, 2, 2}}
+    function SLoopTNR(T::TT; gradalg = LBFGS(10; verbosity = 0, gradtol = 6.0e-7, maxiter = 40000)) where {E, S, TT <: AbstractTensorMap{E, S, 4, 0}}
         return new{E, S, TT}(T, gradalg)
     end
 end
