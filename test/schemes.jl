c1998acdd9d22ffa7fbf9f66e368040a324fb6d5
Adwait-Naravane <adwaitnaravane@gmail.com>
2025-11-29 16:29:06 +0100

    Fix tests

--- test/schemes.jl
+++ test/schemes.jl
@@ -178,13 +178,13 @@ end
     @info "LoopTNR ising ground state degeneracy"
     T1 = classical_ising_symmetric(ising_βc - 0.01)
     scheme = LoopTNR(T1)
-    run!(scheme, truncdim(12), maxiter(10))
+    run!(scheme, truncdim(12), maxiter(20))
     gsd = ground_state_degeneracy(scheme)
     @test gsd ≈ 1 rtol = 1.0e-2
 
     T2 = classical_ising_symmetric(ising_βc + 0.01)
     scheme = LoopTNR(T2)
-    run!(scheme, truncdim(12), maxiter(10))
+    run!(scheme, truncdim(12), maxiter(20))
     gsd = ground_state_degeneracy(scheme)
     @test gsd ≈ 2 rtol = 1.0e-2
 end
