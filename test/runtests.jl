057e0bc8ffc8de095aa1af2bb7392519e3b9fe4e
Chenqi Meng <mcqgravity@gmail.com>
2026-01-07 16:20:06 +0800

    Added tests of free energy and added reference

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,6 @@ using QuadGK
 include("spaces.jl") # do they give spacemismatches?
 include("schemes.jl") # do they give the correct results (with the expected accuracy)?
 include("schemes_triangular.jl") # do they give the correct results (with the expected accuracy)?
+include("schemes_kagome.jl")
 include("models.jl") # do they give the correct results (with the expected accuracy)?
 include("fermions.jl") # do they give the correct results (with the expected accuracy)?
---
+++ test/schemes_kagome.jl
@@ -0,0 +1,17 @@
+@testset "LoopTNR on Kagome lattice" begin
+    T_ising_dual_triangular = classical_ising_dual_triangular()
+    TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
+    scheme = KagomeLoopTNR(TA, TB, TC)
+    data = run!(scheme, truncdim(10), maxiter(15); verbosity = 3, max_loop = 20)
+    fs = free_energy(data, ising_βc_triangular; scalefactor = 3.0)
+    @test fs ≈ f_onsager_triangular rtol = 1.0e-5
+end
+
+@testset "LoopTNR on Kagome lattice with Z2 symmetry" begin
+    T_ising_dual_triangular = classical_ising_dual_triangular_symmetric()
+    TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
+    scheme = KagomeLoopTNR(TA, TB, TC)
+    data = run!(scheme, truncdim(10), maxiter(15); verbosity = 3, max_loop = 20)
+    fs = free_energy(data, ising_βc_triangular; scalefactor = 3.0)
+    @test fs ≈ f_onsager_triangular rtol = 1.0e-5
+end
