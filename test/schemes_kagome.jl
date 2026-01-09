d365f6c048154be160900689ff0e3ba24556a975
Chenqi Meng <mcqgravity@gmail.com>
2026-01-10 02:52:14 +0800

    Try to debug by changing CI

--- test/schemes_kagome.jl
+++ test/schemes_kagome.jl
@@ -13,13 +13,13 @@
     T_ising_dual_triangular = classical_ising_dual_triangular_symmetric()
     TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
     scheme = KagomeLoopTNR(TA, TB, TC)
-    data = run!(scheme, truncdim(5), maxiter(2); max_loop = 3, verbosity = 3)
+    # data = run!(scheme, truncdim(5), maxiter(2); max_loop = 3, verbosity = 3)
 
     # @info "Test free energy"
     # fs = free_energy(data, ising_βc_triangular; scalefactor = 3.0)
     # @test fs ≈ f_onsager_triangular rtol = 1.0e-6
 
-    @info "Test CFT data"
+    # @info "Test CFT data"
     @show scheme
     cft = cft_data!(scheme, [3 / 2, 2 * sqrt(3), 1 / 4])
     d1, d2 = real(cft[Z2Irrep(1)][1]), real(cft[Z2Irrep(0)][2])
