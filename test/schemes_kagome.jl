d3a2b1a6ea378a33f98bd23face11b0a6f3d846e
Chenqi Meng <mcqgravity@gmail.com>
2026-01-09 16:24:21 +0800

    Update schemes_kagome.jl

--- test/schemes_kagome.jl
+++ test/schemes_kagome.jl
@@ -1,13 +1,13 @@
-# @testset "Ising model test of LoopTNR on Kagome lattice" begin
-#     T_ising_dual_triangular = classical_ising_dual_triangular()
-#     TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
-#     scheme = KagomeLoopTNR(TA, TB, TC)
-#     data = run!(scheme, truncdim(8), maxiter(10); max_loop=20, verbosity = 3)
+@testset "Ising model test of LoopTNR on Kagome lattice" begin
+    T_ising_dual_triangular = classical_ising_dual_triangular()
+    TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
+    scheme = KagomeLoopTNR(TA, TB, TC)
+    data = run!(scheme, truncdim(8), maxiter(10); max_loop=20, verbosity = 3)
 
-#     @info "Test free energy"
-#     fs = free_energy(data, ising_βc_triangular; scalefactor=3.0)
-#     @test fs ≈ f_onsager_triangular rtol = 1.0e-4
-# end
+    @info "Test free energy"
+    fs = free_energy(data, ising_βc_triangular; scalefactor=3.0)
+    @test fs ≈ f_onsager_triangular rtol = 1.0e-4
+end
 
 @testset "LoopTNR on Kagome lattice with Z2 symmetry" begin
     T_ising_dual_triangular = classical_ising_dual_triangular_symmetric()
