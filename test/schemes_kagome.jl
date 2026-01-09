dadfe0152d4df3f44e669f06fbc21a9c9ee7690d
Chenqi Meng <mcqgravity@gmail.com>
2026-01-09 17:57:18 +0800

    Update schemes_kagome.jl

--- test/schemes_kagome.jl
+++ test/schemes_kagome.jl
@@ -23,6 +23,6 @@ end
     cft = cft_data!(scheme, [3 / 2, 2 * sqrt(3), 1 / 4])
     d1, d2 = real(cft[Z2Irrep(1)][1]), real(cft[Z2Irrep(0)][2])
     @info "Obtained lowest scaling dimensions:\n$(d1), $(d2)."
-    @test d1 ≈ ising_cft_exact[1] rtol = 1.0e-3
+    @test d1 ≈ ising_cft_exact[1] rtol = 5.0e-3
     @test d2 ≈ ising_cft_exact[2] rtol = 5.0e-4
 end
