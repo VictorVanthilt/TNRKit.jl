5fbe602fb531b9325f775d934966c524004db729
Chenqi Meng <mcqgravity@gmail.com>
2026-01-11 18:59:13 +0800

    Update schemes_kagome.jl

--- test/schemes_kagome.jl
+++ test/schemes_kagome.jl
@@ -2,10 +2,10 @@
     T_ising_dual_triangular = classical_ising_dual_triangular()
     TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
     scheme = KagomeLoopTNR(TA, TB, TC)
-    data = run!(scheme, truncdim(8), maxiter(10); max_loop = 20, verbosity = 3)
+    data = run!(scheme, truncdim(8), maxiter(10); max_loop = 20, verbosity = 1)
 
     @info "Test free energy"
-    fs = free_energy(data, ising_βc_triangular; scalefactor = 1.0)
+    fs = free_energy(data, ising_βc_triangular; scalefactor = 3.0)
     @test fs ≈ f_onsager_triangular rtol = 1.0e-4
 end
 
@@ -13,10 +13,10 @@ end
     T_ising_dual_triangular = classical_ising_dual_triangular_symmetric()
     TA, TB, TC = honeycomb_to_kagome(T_ising_dual_triangular)
     scheme = KagomeLoopTNR(TA, TB, TC)
-    data = run!(scheme, truncdim(16), maxiter(12); max_loop = 30, verbosity = 3)
+    data = run!(scheme, truncdim(16), maxiter(12); max_loop = 30, verbosity = 1)
 
     @info "Test free energy"
-    fs = free_energy(data, ising_βc_triangular; scalefactor = 1.0)
+    fs = free_energy(data, ising_βc_triangular; scalefactor = 3.0)
     @test fs ≈ f_onsager_triangular rtol = 1.0e-6
 
     @info "Test CFT data"
