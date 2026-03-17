366d0ea0e821665878f9b028ceb89f4c201daa8f
Boris De Vos <boris.devos@ugent.be>
2026-03-17 12:13:52 +0100

    potts

--- src/models/potts.jl
+++ src/models/potts.jl
@@ -1,107 +1,90 @@
 """
+    weyl_heisenberg_matrices(dimension [, eltype])
+
+the Weyl-Heisenberg matrices according to [Wikipedia](https://en.wikipedia.org/wiki/Generalizations_of_Pauli_matrices#Sylvester's_generalized_Pauli_matrices_(non-Hermitian)).
+"""
+function weyl_heisenberg_matrices(q::Int, elt::Type{<:Number} = ComplexF64)
+    U = zeros(elt, q, q) # clock matrix
+    V = zeros(elt, q, q) # shift matrix
+    W = zeros(elt, q, q) # DFT
+    ω = cis(2 * pi / q)
+
+    for row in 1:q
+        U[row, row] = ω^(row - 1)
+        V[row, mod1(row - 1, q)] = one(elt)
+        for col in 1:q
+            W[row, col] = ω^((row - 1) * (col - 1))
+        end
+    end
+    return U, V, W / sqrt(q)
+end
+
+function potts_tensor(q::Int, β::Real)
+    A_potts = zeros(Float64, q, q, q, q)
+    for i in 1:q, j in 1:q, k in 1:q, l in 1:q
+        E = -(Int(i == j) + Int(j == l) + Int(k == l) + Int(k == i))
+        A_potts[i, j, k, l] = exp(-β * E)
+    end
+    P = ℂ^q
+    return TensorMap(A_potts, P ⊗ P ← P ⊗ P)
+end
+
+"""
 $(SIGNATURES)
 
 returns the inverse critical temperature for the classical q-state Potts model on a 2D square lattice.
 
-See also: [`classical_potts`](@ref), [`classical_potts_symmetric`](@ref).
+See also: [`classical_potts`](@ref).
 """
 potts_βc(q) = log(1.0 + sqrt(q))
 
 """
-$(SIGNATURES)
+    classical_potts(::Type{Trivial}, q::Int, β::Float64)
+    classical_potts(::Type{ZNIrrep{N}}, q::Int, β::Float64) where {N}
 
 Constructs the partition function tensor for the classical Potts model with `q` states
 and a given inverse temperature `β`.
 
+Compatible with no symmetry or with explicit ℤq symmetry on each of its spaces.
+Defaults to ℤq symmetry if the symmetry type is not provided.
+
 ### Examples
 ```julia
-    classical_potts(3) # Default inverse temperature is `potts_βc(3)`
-    classical_potts(3, 0.5) # Custom inverse temperature.
+    classical_potts(3) # Default has Z₃ symmetry and uses `potts_βc(3)` as the inverse temperature.
+    classical_potts(Z3Irrep, 3, 0.5) # Custom inverse temperature with explicit ℤ₃ symmetry.
 ```
 
-See also: [`classical_potts_symmetric`](@ref), [`potts_βc`](@ref).
+See also: [`potts_βc`](@ref).
 """
-function classical_potts(q::Int, β::Float64)
-    V = ℂ^q
-    A_potts = zeros(Float64, V ⊗ V ← V ⊗ V)
-
-    for i in 1:q
-        for j in 1:q
-            for k in 1:q
-                for l in 1:q
-                    E = -(Int(i == j) + Int(j == l) + Int(l == k) + Int(k == i))
-                    A_potts[i, j, k, l] = exp(-β * E)
-                end
-            end
-        end
-    end
-    return A_potts
+function classical_potts(q::Int, β::Real)
+    return classical_potts(ZNIrrep{q}, q, β)
 end
+classical_potts(::Type{Trivial}, q::Int64) = classical_potts(Trivial, q, potts_βc(q))
 classical_potts(q::Int) = classical_potts(q, potts_βc(q))
-
-function weyl_heisenberg_matrices(Q::Int, elt = ComplexF64)
-    U = zeros(elt, Q, Q) # clock matrix
-    V = zeros(elt, Q, Q) # shift matrix
-    W = zeros(elt, Q, Q) # DFT
-    ω = cis(2 * pi / Q)
-
-    for row in 1:Q
-        U[row, row] = ω^(row - 1)
-        V[row, mod1(row - 1, Q)] = one(elt)
-        for col in 1:Q
-            W[row, col] = ω^((row - 1) * (col - 1))
-        end
-    end
-    return U, V, W / sqrt(Q)
+function classical_potts(::Type{Trivial}, q::Int, β::Real)
+    return potts_tensor(q, β)
 end
+function classical_potts(::Type{ZNIrrep{N}}, q::Int64, β::Real) where {N}
+    @assert N == q "number of irreps must match the number of states"
+    A = potts_tensor(q, β)
 
-"""
-$(SIGNATURES)
-
-Constructs the partition function tensor for a symmetric Potts model with `q` states
-and a given inverse temperature `β`.
-
-This tensor has explicit ℤq symmetry on each of its spaces.
-
-### Examples
-```julia
-    classical_potts_symmetric(3) # Default inverse temperature is `potts_βc(3)`
-    classical_potts_symmetric(3, 0.5) # Custom inverse temperature.
-```
-
-See also: [`classical_potts`](@ref), [`potts_βc`](@ref).
-"""
-function classical_potts_symmetric(q::Int64, β::Float64)
-    V = ℂ^q
-    A_potts = zeros(Float64, V ⊗ V ← V ⊗ V)
-    for i in 1:q
-        for j in 1:q
-            for k in 1:q
-                for l in 1:q
-                    E = -(Int(i == j) + Int(j == l) + Int(k == l) + Int(k == i))
-                    A_potts[i, j, k, l] = exp(-β * E)
-                end
-            end
-        end
-    end
-    Vp = Vect[ZNIrrep{q}](sector => 1 for sector in 0:(q - 1))
     _, _, W = weyl_heisenberg_matrices(q)
     P = TensorMap(W, ℂ^q ← ℂ^q)
-    A_potts = TensorMap(
-        reshape(((P' ⊗ P') * A_potts * (P ⊗ P)).data, (q, q, q, q)),
-        Vp ⊗ Vp ← Vp ⊗ Vp
-    )
+    Udat = reshape(((P' ⊗ P') * A * (P ⊗ P)).data, (q, q, q, q))
+    Vp = Vect[ZNIrrep{q}](sector => 1 for sector in 0:(q - 1))
+    A_potts = TensorMap(real(Udat), Vp ⊗ Vp ← Vp ⊗ Vp)
     return A_potts
 end
-classical_potts_symmetric(q::Int) = classical_potts_symmetric(q, potts_βc(q))
-
 
 """
-$(SIGNATURES)
+    classical_potts_impurity(q::Int64, β::Real, k1::Int64 = 1, k2::Int64 = 1)
+    classical_potts_impurity(q::Int64, k1::Int64, k2::Int64)
+    classical_potts_impurity(q::Int64)
 
 Constructs the partition function tensor for a Potts model with `q` states
 and a given inverse temperature `β` with impurities in sectors `k1` and `k2`.
 
+Currently only implemented without symmetry, but the impurity sectors match the ℤq symmetry sectors of the model.
 ### Examples
 ```julia
     classical_potts_impurity(3) # Default inverse temperature is `potts_βc(3)`
@@ -111,7 +94,12 @@ and a given inverse temperature `β` with impurities in sectors `k1` and `k2`.
 
 See also: [`classical_potts`](@ref), [`potts_βc`](@ref).
 """
-function classical_potts_impurity(q::Int64, β::Float64, k1::Int64 = 1, k2::Int64 = 1)
+function classical_potts_impurity(q::Int64, β::Real, k1::Int64 = 1, k2::Int64 = 1)
+    return classical_potts_impurity(Trivial, q, β, k1, k2)
+end
+classical_potts_impurity(q::Int64, k1::Int64, k2::Int64) = classical_potts_impurity(q, potts_βc(q), k1, k2)
+classical_potts_impurity(q::Int64) = classical_potts_impurity(q, potts_βc(q), 1, 1)
+function classical_potts_impurity(::Type{Trivial}, q::Int64, β::Real, k1::Int64 = 1, k2::Int64 = 1)
     bond_tensor = zeros(Float64, q, q)
     for i in 0:(q - 1)
         bond_tensor[i + 1, i + 1] = sqrt(exp(β) - 1 + q * (i == 0))
@@ -131,6 +119,3 @@ function classical_potts_impurity(q::Int64, β::Float64, k1::Int64 = 1, k2::Int6
 
     return T
 end
-
-classical_potts_impurity(q::Int64, k1::Int64, k2::Int64) = classical_potts_impurity(q, potts_βc(q), k1, k2)
-classical_potts_impurity(q::Int64) = classical_potts_impurity(q, potts_βc(q), 1, 1)
